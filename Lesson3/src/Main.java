ae57590973d149ea73ae20664368eac55577e5d7
unknown <lobakin_vova@vk.com>
2024-11-14 16:35:31 +0400

    Add check to phone number and gender

--- Lesson3/src/Main.java
+++ Lesson3/src/Main.java
@@ -25,26 +25,63 @@ public class Main {
                     System.exit(0);
                 } else if (response.equalsIgnoreCase("y")) {
                     System.out.println("Введите данные в формате : dd.MM.yyyy");
-                    userData [3] = scanner.nextLine();
+                    userData[3] = scanner.nextLine();
                 } else {
                     System.out.println("Неправильный ввод. Пожалуйста, введите 'y' или 'n'.");
-                }1
+                }
             }
         }
 
-        String lastName = userData[0];
-        String firstName = userData[1];
-        String middleName = userData[2];
-        String birthDate = userData[3];
-        String phoneNumber = userData[4];
-        String gender = userData[5];
+        while (true) {
+            date = userData[4];
 
-        System.out.println("Фамилия: " + lastName);
-        System.out.println("Имя: " + firstName);
-        System.out.println("Отчество: " + middleName);
-        System.out.println("Дата рождения: " + birthDate);
-        System.out.println("Номер телефона: " + phoneNumber);
-        System.out.println("Пол: " + gender);
+            if (isValidNumber(date)) {
+                break;
+            } else {
+                System.out.println("Номер введен не корректно: " + date);
+                System.out.println("Хотите ввести номер заново? (y/n)");
+                String response = scanner.nextLine();
+
+                if (response.equalsIgnoreCase("n")) {
+                    System.out.println("Завершение программы");
+                    System.exit(0);
+                } else if (response.equalsIgnoreCase("y")) {
+                    System.out.println("Введите номер. Он должен быть в формате long  не отрицательным");
+                    userData[4] = scanner.nextLine();
+                } else {
+                    System.out.println("Неправильный ввод. Пожалуйста, введите 'y' или 'n'.");
+                }
+            }
+        }
+
+        while (true) {
+            date = userData[5];
+
+            if (isValidGender(date)) {
+                break;
+            } else {
+                System.out.println("Пол введен неверно: " + date);
+                System.out.println("Хотите ввести пол заново? (y/n)");
+                String response = scanner.nextLine();
+
+                if (response.equalsIgnoreCase("n")) {
+                    System.out.println("Завершение программы");
+                    System.exit(0);
+                } else if (response.equalsIgnoreCase("y")) {
+                    System.out.println("Для выбора мужского пола введите m, для женского f");
+                    userData[5] = scanner.nextLine();
+                } else {
+                    System.out.println("Неправильный ввод. Пожалуйста, введите 'y' или 'n'.");
+                }
+            }
+        }
+
+        System.out.println("Фамилия: " + userData[0]);
+        System.out.println("Имя: " + userData[1]);
+        System.out.println("Отчество: " + userData[2]);
+        System.out.println("Дата рождения: " + userData[3]);
+        System.out.println("Номер телефона: " + userData[4]);
+        System.out.println("Пол: " + userData[5]);
 
         scanner.close();
     }
@@ -89,4 +126,39 @@ public class Main {
             return false;
         }
     }
+
+
+    public static boolean isValidNumber(String telNum) {
+        String input = telNum;
+        try {
+            long result = Long.parseLong(input);
+            if (result > 0) {
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return false;
+    }
+
+
+    public static boolean isValidGender(String gender) {
+        String input = gender;
+        try {
+            if (input.length() != 1) {
+                throw new Exception("Ввод должен состоять из одного символа.");
+            }
+
+            char letter = input.charAt(0);
+            if (letter == 'f' || letter == 'm') {
+                return true;
+            } else {
+                throw new Exception("Введена неверная буква. Пожалуйста, введите 'f' или 'm'.");
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
+
+
